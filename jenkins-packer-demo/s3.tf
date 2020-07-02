ed976027c3b68483d723f67242dc467f2fa3b206
mk250101 <mahesh.kharwadkar@teradata.com>
2020-07-02 15:54:43 +0530

    add

--- jenkins-packer-demo/s3.tf
+++ jenkins-packer-demo/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "terraform-state" {
   bucket = "terraform-state-${random_string.random.result}"
   acl    = "private"
+  region = "us-east-1"
 
   tags = {
     Name = "Terraform state"
