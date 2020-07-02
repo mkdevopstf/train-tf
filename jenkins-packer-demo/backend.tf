52dc6beb65ad68b1f50b4e38144104e20f3c52e0
mk250101 <mahesh.kharwadkar@teradata.com>
2020-07-02 15:38:54 +0530

    code coment backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-xx70dpnh1"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-xx70dpnh1"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
